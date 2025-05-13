5fd97e4aead4b7b8b71b5212acadf1fab1363d3b
nnamanjjain <nnamanjjain@gmail.com>
2025-05-13 12:08:23 +0530

    commit-assign-1

---
+++ Task-1.py
@@ -0,0 +1,12 @@
+#Task-1: Perform Basic Mathematical Operations:
+
+a=input("Enter the first number: ")
+b=input("Enter the second number: ")
+
+c=float(a)
+d=float(b)
+
+print('The Addition of numbers '+a+' and '+b+ ' is: ',c+d)
+print('The Difference of numbers '+a+' and '+b+ ' is: ',abs(c-d))
+print('The Multiplication of numbers '+a+' and '+b+ ' is: ',c*d)
+print('The Division of numbers '+a+' and '+b+ ' is: ',d/c)
---
+++ Task-2.py
@@ -0,0 +1,6 @@
+#Task-2: Create a Personalized Greeting
+
+a=input('Enter your first name: ')
+b=input('Enter your Last name: ')
+
+print('Hello, '+a+' '+b+'! Welcome to the Python Program.')
